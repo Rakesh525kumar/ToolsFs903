e21c058c9cd7e9e0573c5a55dd4d67d04f9b0337
HP <HP@LAPTOP-C5AHSE4N>
2024-11-08 14:25:24 +0530

    Task completed

--- Gitproj1/src/main/java/com/nt/App.java
+++ Gitproj1/src/main/java/com/nt/App.java
@@ -3,6 +3,19 @@ package com.nt;
 
 public class App {
 	
+	void sum()
+	{
+		int a = 100;
+		int b= 200;
+		System.out.println(a+b);
+	}
+	
+	public static void main(String[] args) {
+		App a = new App();
+		a.sum();
+		
+	}
+	
 	
 	
     }
