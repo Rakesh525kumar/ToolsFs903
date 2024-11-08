9cfe7066933759af17edfc79f8120f03aa44c797
HP <HP@LAPTOP-C5AHSE4N>
2024-11-08 14:00:39 +0530

    TL project created

--- Gitproj1/src/main/java/com/nt/App.java
+++ Gitproj1/src/main/java/com/nt/App.java
@@ -4,14 +4,5 @@ package com.nt;
 public class App {
 	
 	
-	void Sum(int a, int b)
-	{
-		
-		System.out.println(a+ b);
-	}
-    public static void main(String[] args) {
-      
-    	App a = new App();
-    	a.Sum(10,20);
+	
     }
-}
